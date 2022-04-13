9a9d91033cecd0ca89557f5a62ff9d659ec00f65
alfonsomendezc <alfonsomendez2209@gmail.com>
2022-04-13 00:33:39 +0000

    v1

--- src/app.js
+++ src/app.js
@@ -2,10 +2,40 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add(generadorDeIconos());
+  document.querySelector(".number").innerHTML = generadorDeNumeros();
+  document.querySelector(".top-suit").innerHTML = generadorDeSuits();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generadorDeNumeros = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indiceN = ~~(Math.random() * numbers.length);
+  return numbers[indiceN];
+};
+
+let generadorDeIconos = () => {
+  let iconos = ["spades", "clubs", "hearts", "diamonds"];
+  let indiceI = ~~(Math.random() * iconos.length);
+  return iconos[indiceI];
+};
+
+let generadorDeSuits = () => {
+  let iconos = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  let indiceI = ~~(Math.random() * iconos.length);
+  return iconos[indiceI];
 };
